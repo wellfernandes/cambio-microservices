b741cb03bd86d636f0d5df2a58fa5a69b54af791
Welliton Fernandes Leal <wellfernandes@me.com>
2024-04-06 00:08:49 -0300

    feat: added cambio controller with mock to test

---
+++ cambio-service/src/main/java/br/com/wellitonleal/cambioservice/controller/CambioController.java
@@ -0,0 +1,24 @@
+package br.com.wellitonleal.cambioservice.controller;
+
+import br.com.wellitonleal.cambioservice.model.Cambio;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+
+    @GetMapping(value = "/{amount}/{from}/{to}")
+    public Cambio getCambio(
+            @PathVariable("amount") BigDecimal amount,
+            @PathVariable("from") String from,
+            @PathVariable("to") String to
+    ){
+
+        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, "PORT 8000");
+    }
+}
