e122156f15c5471f729679a0d414f1f64422eb70
Welliton Fernandes Leal <wellfernandes@me.com>
2024-04-05 21:07:12 -0300

    feat: cambio service project created

---
+++ cambio-service/src/test/java/br/com/wellitonleal/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wellitonleal.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
