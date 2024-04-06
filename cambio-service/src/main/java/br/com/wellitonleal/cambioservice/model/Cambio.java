ea727cb77b43ac3081f2f5abfe8453fc6b4f8526
Welliton Fernandes Leal <wellfernandes@me.com>
2024-04-06 00:08:17 -0300

    feat: added cambio model

---
+++ cambio-service/src/main/java/br/com/wellitonleal/cambioservice/model/Cambio.java
@@ -0,0 +1,102 @@
+package br.com.wellitonleal.cambioservice.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Cambio implements Serializable {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionFactor;
+    private BigDecimal convertedValue;
+    private String environment;
+
+    public Cambio(){}
+
+    public Cambio(Long id, String from, String to, BigDecimal conversionFactor, BigDecimal convertedValue, String environment) {
+        this.id = id;
+        this.from = from;
+        this.to = to;
+        this.conversionFactor = conversionFactor;
+        this.convertedValue = convertedValue;
+        this.environment = environment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public BigDecimal getConversionFactor() {
+        return conversionFactor;
+    }
+
+    public void setConversionFactor(BigDecimal conversionFactor) {
+        this.conversionFactor = conversionFactor;
+    }
+
+    public BigDecimal getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void setConvertedValue(BigDecimal convertedValue) {
+        this.convertedValue = convertedValue;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cambio cambio = (Cambio) o;
+
+        if (!Objects.equals(id, cambio.id)) return false;
+        if (!Objects.equals(from, cambio.from)) return false;
+        if (!Objects.equals(to, cambio.to)) return false;
+        if (!Objects.equals(conversionFactor, cambio.conversionFactor))
+            return false;
+        if (!Objects.equals(convertedValue, cambio.convertedValue))
+            return false;
+        return Objects.equals(environment, cambio.environment);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (from != null ? from.hashCode() : 0);
+        result = 31 * result + (to != null ? to.hashCode() : 0);
+        result = 31 * result + (conversionFactor != null ? conversionFactor.hashCode() : 0);
+        result = 31 * result + (convertedValue != null ? convertedValue.hashCode() : 0);
+        result = 31 * result + (environment != null ? environment.hashCode() : 0);
+        return result;
+    }
+}
