effb4ad02f4ee986e2c571f298db5b5e44ac46b9
Welliton Fernandes Leal <wellfernandes@me.com>
2024-04-13 22:50:23 -0300

    refactor: added cambio respository

--- cambio-service/src/main/java/br/com/wellitonleal/cambioservice/controller/CambioController.java
+++ cambio-service/src/main/java/br/com/wellitonleal/cambioservice/controller/CambioController.java
@@ -1,6 +1,7 @@
 package br.com.wellitonleal.cambioservice.controller;
 
 import br.com.wellitonleal.cambioservice.model.Cambio;
+import br.com.wellitonleal.cambioservice.repository.CambioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @RestController
 @RequestMapping("cambio-service")
@@ -16,6 +18,7 @@ public class CambioController {
 
     @Autowired
     private Environment environment;
+    private CambioRepository cambioRepository;
 
     @GetMapping(value = "/{amount}/{from}/{to}")
     public Cambio getCambio(
@@ -23,8 +26,16 @@ public class CambioController {
             @PathVariable("from") String from,
             @PathVariable("to") String to
     ){
+        var cambio = cambioRepository.findByFromAndTo(from, to);
+        if(cambio == null) throw new RuntimeException("Currency Unsupported");
 
         var port = environment.getProperty("local.server.port");
-        return new Cambio(1L, from, to, BigDecimal.ONE, BigDecimal.ONE, port);
+        cambio.setEnvironment(port);
+
+        BigDecimal conversionFactor = cambio.getConversionFactor();
+        BigDecimal convertedValue = conversionFactor.multiply(amount);
+        cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+
+        return cambio;
     }
 }
