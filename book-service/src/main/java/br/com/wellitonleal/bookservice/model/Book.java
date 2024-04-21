d65ae819dac50acdb1e3b39c806d95107589ce65
Welliton Fernandes Leal <wellfernandes@me.com>
2024-04-21 20:45:38 -0300

    feat: added spring annotations for persistence

--- book-service/src/main/java/br/com/wellitonleal/bookservice/model/Book.java
+++ book-service/src/main/java/br/com/wellitonleal/bookservice/model/Book.java
@@ -1,18 +1,35 @@
 package br.com.wellitonleal.bookservice.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+@Entity(name = "book")
 public class Book implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "author", nullable = false, length = 180)
     private String author;
+
+    @Column(name = "title", nullable = false, length = 250)
     private String title;
+
+    @Column(name = "launch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date launchDate;
+
+    @Column(name = "price", nullable = false)
     private Double price;
 
+    @Transient
     private String currency;
+
+    @Transient
     private String environment;
 
     public Book() {
